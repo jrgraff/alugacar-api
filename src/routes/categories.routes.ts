85512c74f0e49b79d149910f072521ff6fbc16c6
Jackson Graff <jacksongraff@hotmail.com>
2021-03-15 15:15:37 -0300

    added: category get and post

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,22 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  categoriesRepository.create({ name, description });
+  return response.status(201).send();
+});
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
 
-  categories.push(category);
-
-  return response.status(201).json(category);
+  return response.status(201).json(all);
 });
 
 export { categoriesRoutes };
