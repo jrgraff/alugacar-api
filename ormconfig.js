6ee17b5e3631b3dbb564493e951a385e9543313a
Jackson Graff <jacksongraff@hotmail.com>
2021-04-06 09:24:29 -0300

    feat: create cars

--- ormconfig.js
+++ ormconfig.js
@@ -6,7 +6,7 @@ module.exports = {
   password: 'docker',
   database: 'rentx',
   entities: ['./src/modules/**/entities/*.ts'],
-  migrations: ['./src/shared/infra/typeorm/migrations*.ts'],
+  migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
   cli: {
     migrationsDir: './src/shared/infra/typeorm/migrations',
   },
